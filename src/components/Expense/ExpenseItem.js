83594129928c65ecf24b46aea28b54487f39d63d
roshinys <roshinchaya@gmail.com>
2023-03-27 20:37:28 +0530

    added price state using useState

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 function ExpenseItem({ expense }) {
+  const [title, setTitle] = useState(expense.name);
+  const [price, setPrice] = useState(expense.price);
+
   const clickHandler = () => {
-    console.log("clicked!");
+    // title = "UpdatedTitle";
+    setTitle("UpdatedTitle");
+    console.log(title);
   };
   const deleteHandler = (e) => {
     console.log("delete this expense");
@@ -15,21 +20,25 @@ function ExpenseItem({ expense }) {
     const parentExpense = expenseItem.parentElement;
     parentExpense.removeChild(expenseItem);
   };
+  const setpriceHandler = () => {
+    setPrice(100);
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={expense.date} />
       <div className="expense-item__description">
         <ExpenseDetails
-          name={expense.name}
+          name={title}
           location={expense.location}
-          price={expense.price}
+          price={price}
         />
       </div>
       <button onClick={clickHandler}>Change Title</button>
       <button onClick={deleteHandler} id={expense.id}>
         Delete Expense
       </button>
+      <button onClick={setpriceHandler}>add 100rs</button>
     </Card>
   );
 }
