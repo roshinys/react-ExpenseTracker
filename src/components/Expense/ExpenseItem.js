d45a7f2b03b839d328303a26f89f0c7a5e2c7050
roshinys <roshinchaya@gmail.com>
2023-03-27 19:49:14 +0530

    added delete expense item using dom manipulation

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -5,6 +5,17 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 function ExpenseItem({ expense }) {
+  const clickHandler = () => {
+    console.log("clicked!");
+  };
+  const deleteHandler = (e) => {
+    console.log("delete this expense");
+    const expenseId = e.target.id;
+    const expenseItem = document.getElementById(expenseId).parentElement;
+    const parentExpense = expenseItem.parentElement;
+    parentExpense.removeChild(expenseItem);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={expense.date} />
@@ -15,6 +26,10 @@ function ExpenseItem({ expense }) {
           price={expense.price}
         />
       </div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={deleteHandler} id={expense.id}>
+        Delete Expense
+      </button>
     </Card>
   );
 }
