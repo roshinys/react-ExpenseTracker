678cb8a28ba0edcbb83dbc8883edf6dac1a264c6
roshinys <roshinchaya@gmail.com>
2023-05-08 20:58:01 +0530

    added form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import React from "react";
 import Expense from "./components/Expense/Expense";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   return (
     <div className="app">
+      <NewExpense />
       <Expense />
     </div>
   );
